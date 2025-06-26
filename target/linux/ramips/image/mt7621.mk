29c31e693c1b9e5dee659b4de7f4008787e89fee
iuncuim <iuncuim@gmail.com>
2025-06-26 14:36:37 +0800

    system: release 1.1.2s

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2321,10 +2321,10 @@ define Device/teleofis_lt70
   DEVICE_VENDOR := Teleofis
   DEVICE_MODEL := LT70
   DEVICE_PACKAGES := kmod-sdhci-mt7620 \
-  kmod-pps kmod-pps-ldisc kmod-pps-gpio kmod-usb2 kmod-usb3 \
+  kmod-pps kmod-pps-ldisc kmod-pps-gpio kmod-usb3 \
   kmod-usb-serial kmod-usb-serial-ftdi kmod-usb-serial-cp210x \
   kmod-usb-serial-ch341 kmod-usb-serial-option kmod-usb-net-rndis kmod-usb-net-qmi-wwan \
-  kmod-button-hotplug kmod-mtd-rw \
+  kmod-button-hotplug kmod-mtd-rw kmod-wireguard \
   kmod-usb-storage kmod-usb-storage-extras kmod-usb-acm \
   kmod-fs-exfat kmod-fs-ext4 kmod-fs-f2fs kmod-fs-vfat kmod-fs-ntfs kmod-fs-nfs \
   kmod-ath10k ath10k-firmware-qca6174-fn-link-8274b-pr \
